1310942f68887a6c1686b74401b4162410d82806
TMINT DEV <gondetgendat@gmail.com>
2026-07-06 09:39:38 +0700

    fix: correct typos

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  console.log(process.env.FRONTEND_URL);
+
   app.enableCors({
     origin: process.env.FRONTEND_URL,
-    credential: true, 
+    credentials: true,
   });
 
   await app.listen(process.env.PORT ?? 3001);
