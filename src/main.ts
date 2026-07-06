5390f923f229b5fa31faa6a395b4e0217d2c602f
TMINT DEV <gondetgendat@gmail.com>
2026-07-06 10:04:04 +0700

    fix: allow localhost origin in CORS config

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  console.log(process.env.FRONTEND_URL);
-
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
+    origin: [
+      "http://localhost:3000",
+      process.env.FRONTEND_URL,
+    ],
+    credentials: true, 
   });
 
   await app.listen(process.env.PORT ?? 3001);
